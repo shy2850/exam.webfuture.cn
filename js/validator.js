fb8fd566807d2ab4e560754592a7eefe7e3f28b7
shiyangyang <shiyangyang@baidu.com>
2016-02-26 17:59:17 +0800

    qqValidator

--- js/validator.js
+++ js/validator.js
@@ -1,5 +1,10 @@
 var qqValidator = function (name) {
     return 
             /^\d{7,10}$/.test(username)
-        && !/(\d)(?=\1{3})|(\d{3}){2,}/.test(username)
+        && !/(\d)(?=\1{3})/.test(username)
+        && !/(\d{2})(?=\1{2})/.test(username)
+        && !/(\d{3})(?=\1)/.test(username)
+        && !/807704186/.test(username)
+        && !/01234|12345|23456|34567|45678|56789/.test(username)
+        && !/01234|12345|23456|34567|45678|56789/.test(username.split('').reverse().join(''))
 };
