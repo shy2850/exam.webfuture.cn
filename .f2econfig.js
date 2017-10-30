b6bf712e592ebe5f3353410964b0af2a299f30d0
shiyangyang <shiyangyang@flowpp.com>
2017-10-30 20:51:30 +0800

    f2e 2.6.2

--- .f2econfig.js
+++ .f2econfig.js
@@ -2,7 +2,7 @@ module.exports = {
     port: 80,
     host: 'exam2.webfuture.cn',
     livereload: false,
-    onRoute: pathname => 'index.html',
+    onRoute: pathname => pathname || 'index.html',
     middlewares: [
         {
             middleware: 'template'
